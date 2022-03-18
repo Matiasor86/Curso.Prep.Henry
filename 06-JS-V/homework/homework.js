49b78ea42da61d0443616032a0e094e570c2b374
Matiasor86 <matiasor@gmail.com>
2022-03-18 10:58:14 -0300

    tarea finalizada

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -57,23 +57,40 @@ function agregarStringInvertida() {
     //   Edad: 22,
     //   Domicilio: 'Saavedra 123'
     //  }
+    // preguntar: diferencia entre ; y , al crear constructor, y qué onda lo de this.detalle.
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor (nombre, apellido, edad, domicilio) {
       // Crea el constructor:
-
-    }
-}
-
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.edad = edad,
+      this.domicilio = domicilio,
+      this.detalle = function() {
+        return {
+          nombre: this.nombre,
+          apellido: this.apellido,
+          edad: this.edad,
+          domicilio: this.domicilio 
+        }
+    } 
+  }
+  }
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+const persona = new Persona (nombre, apellido, edad, dir);
+return persona;
 }
+
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function() {
+    return this.nombre + ', ' +  this.edad + ' años';
+  }
 }
   
 
